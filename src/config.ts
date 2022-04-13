8541a9330283a607423c48c85156836fb02eb01b
DEVIL-RO <103538922+DEVIL-RO@users.noreply.github.com>
2022-04-13 12:53:31 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,11 +7,11 @@ export const NFTCollection = 'DEVIL-0398c2';
 export const NFTContract = 'erd185l7k7h4a6aypx32nrwxmmy28n96l5uutcrlckjkvmntyepkhv4q3h734u';
 
 export const network: Dapp.NetworkType = {
-  id: "mainnet",
-  name: "Mainnet",
-  egldLabel: "EGLD",
-  walletAddress: "https://wallet.elrond.com",
-  apiAddress: "https://api.elrond.com",
-  gatewayAddress: "https://gateway.elrond.com",
-  explorerAddress: "http://explorer.elrond.com/",
+  id: 'mainnet',
+  name: 'Mainnet',
+  egldLabel: 'EGLD',
+  walletAddress: 'https://wallet.elrond.com',
+  apiAddress: 'https://api.elrond.com',
+  gatewayAddress: 'https://gateway.elrond.com',
+  explorerAddress: 'http://explorer.elrond.com/',
 };
